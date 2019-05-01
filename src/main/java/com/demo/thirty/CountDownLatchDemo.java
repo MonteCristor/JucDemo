55f7106bbeb2db1dc081bce1cfc43605aee7c728
MonteCristor <shi_wei_h@163.com>
2019-05-01 12:11:29 +0800

    no CountDownLatch

--- src/main/java/com/demo/thirty/CountDownLatchDemo.java
+++ src/main/java/com/demo/thirty/CountDownLatchDemo.java
@@ -1,22 +1,31 @@
 package com.demo.thirty;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * @Author: sw
  * @sidereal_csday: 2019-05-01 11:48
  */
 public class CountDownLatchDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-    for (int i = 1; i <= 6; i++) {
+        closeDoor();
 
-        new Thread(() -> {
-            System.out.println(Thread.currentThread().getName()+ "\t 上完自习，离开教室");
-
-        }, String.valueOf(i)).start();
     }
 
-        System.out.println(Thread.currentThread().getName() + "\t *****************班长最后关门走人");
+    private static void closeDoor() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(6);
 
+        for (int i = 1; i <= 6; i++) {
+
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName()+ "\t 上完自习，离开教室");
+                countDownLatch.countDown();
+            }, String.valueOf(i)).start();
+        }
+
+        countDownLatch.await();
+        System.out.println(Thread.currentThread().getName() + "\t *****************班长最后关门走人");
     }
 }
